e19a906f0f27a14ce56821498a7303d3d04fd2e4
AlpenJodler <dany.meyer@hnu.de>
2023-04-25 13:52:31 +0200

    adapt Text

--- app/src/main/java/com/example/testgit3/MainActivity.java
+++ app/src/main/java/com/example/testgit3/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     //hier kommen Aenderungen vom User2
+    //und eine weitere Aenderung von User1
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
